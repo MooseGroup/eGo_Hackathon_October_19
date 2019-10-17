1ceaf596f99e61fb7791a56fce7e226b4561cdbe
Angelo Cammalleri <angelocammalleri@me.com>
2019-10-17 17:20:29 +0200

    Add placeholder tableview to project.

---
+++ ios/Moose/Moose/BookingTableViewController.swift
@@ -0,0 +1,38 @@
+//
+//  BookingTableViewController.swift
+//  Moose
+//
+//  Created by Angelo Cammalleri on 17.10.19.
+//  Copyright © 2019 Moose. All rights reserved.
+//
+
+import UIKit
+
+class BookingTableViewController: UITableViewController {
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "reuseIdentifier")
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 10
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        
+        cell.textLabel?.text = "Test"
+        
+        return cell
+    }
+}
---
+++ ios/Moose/Moose/ExsistingBookingViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ExsistingBookingViewController.swift
+//  Moose
+//
+//  Created by Angelo Cammalleri on 17.10.19.
+//  Copyright © 2019 Moose. All rights reserved.
+//
+
+import UIKit
+
+class ExsistingBookingViewController: UIViewController {
+    
+    @IBOutlet weak var tableContainer: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Existing Bookings"
+        setUpChildTableView()
+    }
+    
+    fileprivate func setUpChildTableView() {
+        // Create booking table view
+        let bookingTableViewController = BookingTableViewController(nibName: "BookingTableViewController", bundle: nil)
+        
+        // Add its controller as a child so we don't lose the reference!
+        addChild(bookingTableViewController)
+        
+        // Add the view of our table view to our table view container we prepared to host its ui.
+        tableContainer.addSubview(bookingTableViewController.view)
+        bookingTableViewController.didMove(toParent: self)
+    }
+
+    @IBAction func createNewBooking(_ sender: Any) {
+        // TODO: Open Time and Date Selection Screen from here!
+        let timeAndDateViewController = UIViewController()
+        timeAndDateViewController.view.backgroundColor = .white
+        navigationController?.pushViewController(timeAndDateViewController, animated: true)
+    }
+}
