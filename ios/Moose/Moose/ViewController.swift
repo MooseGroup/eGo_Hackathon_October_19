943c0713342cae657b52c36d561cd12a43c3d761
Christian Menschel <christian@tapwork.de>
2019-10-17 13:47:44 +0200

    Add Xcode project

---
+++ ios/Moose/Moose/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Moose
+//
+//  Created by Christian Menschel on 17.10.19.
+//  Copyright © 2019 Moose. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
