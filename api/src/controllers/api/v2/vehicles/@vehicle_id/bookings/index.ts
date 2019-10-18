13d313f46b8c03a1c06179a64ba2528a25c5e8f2
Christian Menschel <christian@tapwork.de>
2019-10-18 15:11:17 +0200

    Add missing )

--- api/src/controllers/api/v2/vehicles/@vehicle_id/bookings/index.ts
+++ api/src/controllers/api/v2/vehicles/@vehicle_id/bookings/index.ts
@@ -273,6 +273,6 @@ export class Controller extends APIv2VehicleControllerBase {
                 return await database.vehicleBookingToJSON(NEW_DOC, db);
             };
            return CREATE_BOOKING();
-        }
+        })
     }
 }
